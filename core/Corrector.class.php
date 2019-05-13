8e8abf2754bf8262e0433b536459790748e5f689
John Wu <524306184@qq.com>
2019-05-13 10:02:24 +0800

    error handle

--- core/Corrector.class.php
+++ core/Corrector.class.php
@@ -3,36 +3,36 @@ namespace EP;
 
 use Exception;
 
-//set_error_handler(function ($errNo, $errStr, $errFile, $errLine) {
-//    var_dump([$errNo, $errStr, $errFile, $errLine]);
-////    exit();
-//});
-//
-//set_exception_handler(function (Exception $e) {
-//    header('Content-type: text/html;');
-//    header("HTTP/1.1 500 Internal Server Error");
-//
-//    // Get the code lines
-//    $numCount = 5;
-//    $lines = [];
-//    if (null !== ($contents = file_get_contents($e->getFile()))) {
-//        $lines = explode("\n", $contents);
-//        $start  = $e->getLine() - $numCount;
-//        $length = 2 * $numCount;
-//        $lines = array_slice($lines, $start, $length, true);
-//    }
-//
-//    $errorData = array(
-//        'msg' => $e->getMessage(),
-//        'path' => $e->getFile(),
-//        'line' => $e->getLine(),
-//        'code' => $lines,
-//        'trace' => $e->getTrace(),
-//    );
-//
-//    require_once(COREPATH . '/templete/Error.php');
-//    exit();
-//});
+set_error_handler(function ($errNo, $errStr, $errFile, $errLine) {
+    var_dump([$errNo, $errStr, $errFile, $errLine]);
+    exit();
+});
+
+set_exception_handler(function (Exception $e) {
+    header('Content-type: text/html;');
+    header("HTTP/1.1 500 Internal Server Error");
+
+    // Get the code lines
+    $numCount = 5;
+    $lines = [];
+    if (null !== ($contents = file_get_contents($e->getFile()))) {
+        $lines = explode("\n", $contents);
+        $start  = $e->getLine() - $numCount;
+        $length = 2 * $numCount;
+        $lines = array_slice($lines, $start, $length, true);
+    }
+
+    $errorData = array(
+        'msg' => $e->getMessage(),
+        'path' => $e->getFile(),
+        'line' => $e->getLine(),
+        'code' => $lines,
+        'trace' => $e->getTrace(),
+    );
+
+    require_once(COREPATH . '/templete/Error.php');
+    exit();
+});
 
 function arrayToString($array) {
     if (is_array($array)){
